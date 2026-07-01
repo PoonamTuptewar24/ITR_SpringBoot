c83e560c8ca7035142c00fe4a2d3c1cdbe66f3cb
Poonam.T <poonamtuptewar@gmail.com>
2026-07-01 21:02:43 +0530

    Create SportConfig.java

---
+++ SportConfig.java
@@ -0,0 +1,13 @@
+package com.hello.Dependancy_demo.config;
+import com.itr.sample.common.Coach;
+import com.itr.sample.common.FootballCoach;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SportConfiguration {
+    @Bean
+    public Coach FootCoach(){
+        return new FootballCoach();
+    }
+}
